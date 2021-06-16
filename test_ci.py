927f15f0e1cc57627da6422211a97ec80514dd67
benraivel <47224831+benraivel@users.noreply.github.com>
2021-06-16 11:05:49 -0400

    na

--- test_ci.py
+++ test_ci.py
@@ -1,17 +1,12 @@
 import nidaqmx
-import pprint
-
-
-pp = pprint.PrettyPrinter(indent=4)
-
 
 with nidaqmx.Task() as task:
-    task.ci_channels.add_ci_pulse_chan_freq("Dev1/ctr1")
+    task.ci_channels.add_ci_freq_chan('NI_PCIe-6351/ctr0')
 
     print('1 Channel 1 Sample Read: ')
     data = task.read()
-    pp.pprint(data)
+    print(data)
 
     print('1 Channel N Samples Read: ')
     data = task.read(number_of_samples_per_channel=8)
-    pp.pprint(data)
+    print(data)
