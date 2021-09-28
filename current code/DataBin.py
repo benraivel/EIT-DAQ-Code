adf719f8916a50a97f50a6748a4f4ed98fc1883d
benraivel <benraivel@gmail.com>
2021-09-28 13:35:40 -0400

    working on databin.py

--- current code/DataBin.py
+++ current code/DataBin.py
@@ -37,7 +37,7 @@ class DataBin:
 
                 # check if the current array can hold the value
                 if new_index >= self.length:
-                    self.expand_array()
+                    self.expand_array(new_index)
 
                 # if a value exists at the location:
                 if self.tally[new_index] != 0:
@@ -60,17 +60,17 @@ class DataBin:
                     self.tally[new_index] = 1
 
             
-    def expand_array(self):
+    def expand_array(self, new_length):
         '''
-        double the size of data/tally array
+        appropriatley increase size of array
         '''
-
+        self.length = new_length+1
         print('Expanded!')
         # create new data array (doubled size)
-        temp_data = np.empty(2*self.length)
+        temp_data = np.empty(self.length)
 
         # create new tally array (doubled size)
-        temp_tally = np.zeros(2*self.length)
+        temp_tally = np.zeros(self.length)
 
         # copy values over
         for i in range(len(self.data)):
@@ -80,17 +80,18 @@ class DataBin:
         # re-assign
         self.data = temp_data
         self.tally = temp_tally
-        self.length = self.length*2
 
 def main():
 
     # create DataBin object
     avg_dat = DataBin()
 
+    all_data = []
+
     for i in range(10):
 
         print('loading run' + str(i) + '.csv:')
-        filestr = 'testdata/set4/run' + str(i) + '.csv'
+        filestr = 'testdata/set2/run' + str(i) + '.csv'
         # import data for testing
         test_data = data.Data(filestr)
         
@@ -101,21 +102,33 @@ def main():
         # select data signal
         data_signal = test_data.select_dimension(0).T[0]
 
+        all_data.append(data_signal)
+
         fit = an.fit_fabry_perot_peaks(fabry_perot, threshold=2)
 
         fit_poly = fit['poly']
 
-        print('fit coeff:' + str(fit_poly.coef))
+        print('fit coeff:' + str(fit_poly.coef) + '\n\n')
 
         avg_dat.insert_set(data_signal, fit_poly)
 
-        if i ==9:
+        if i == 0:
+            plt.subplot(311)
             plt.scatter(fit['freq_data'], data_signal, s = 1)
-            plt.show()
+
+    all_data = np.array(all_data)
+
+    avg = np.average(all_data, axis = 0)
+    idx = np.arange(len(avg))
 
     freq = np.linspace(0, avg_dat.length*avg_dat.bin_size, avg_dat.length)
-    plt.scatter(freq, avg_dat.get_data()[0], s= 1)
+    plt.subplot(312)
+    plt.scatter(freq, avg_dat.get_data()[0], s = 1)
+    plt.subplot(313)
+    plt.scatter(idx, avg, s = 1)
     plt.show()
+    print(np.amax(avg_dat.get_data()[0]))
+    print(np.amax(avg))
 
 # main method for offline testing
 if __name__ == "__main__":
